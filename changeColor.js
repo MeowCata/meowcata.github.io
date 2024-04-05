afa9cd747c6d70214a54e03a328815f64f7fa9d6
DingDang <97514372+MeowCata@users.noreply.github.com>
2024-04-05 09:13:58 +0800

    Add files via upload

---
+++ changeColor.js
@@ -0,0 +1,10 @@
+//ai generated
+			function changeBackgroundColor() {
+				const htmlBody = document.body;
+				if (htmlBody.style.backgroundColor === 'black') {
+					htmlBody.style.backgroundColor = 'white';
+				} else {
+					htmlBody.style.backgroundColor = 'black';
+				}
+
+			}
